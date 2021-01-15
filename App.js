e06e2cf84492210b68b1012e5d0c7cb18742d2d5
Lakitu <krishin901@gmail.com>
2021-01-14 21:19:59 -0500

    Timer and next speech now operate

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import {Timer} from './Timer'
-import {timerStyles} from './styles'
+import { View } from 'react-native';
+import {Main} from './Main'
+import {styles, timerStyles} from './styles'
+import AppLoading from 'expo-app-loading'
+import {useFonts} from "@expo-google-fonts/inter";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'RobotoMono': require('./assets/fonts/RobotoMono-VariableFont_wght.ttf'),
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
-      {/*<Text>Open up App.js to start working on your app!</Text>*/}
-      {/*<StatusBar style="auto" />*/}
-      <Timer time={"6"} styles={timerStyles}/>
+      <Main times = {times[0]} style={timerStyles}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const times = [
+  [ // CDA times
+    ["A1C", 6],["A1 CX", 3], ["N1C", 6],["N1 CX", 3], ["A2C", 6],["A2 CX", 3], ["N2C", 6],["N2 CX", 3],
+    ["N1R", 4], ["A1R", 4], ["N2R", 4], ["A2R", 4]
+  ],
+]
--- styles.js
+++ styles.js
@@ -4,11 +4,33 @@ export const timerStyles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         marginTop: 3,
     },
-    count: { //TODO: make the timer look sexy
-        fontSize: 100,
-        fontFamily: "Impact, sansSerif",
+    count: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        fontFamily: "RobotoMono",
     },
+    finishedRed: {
+        color: 'red',
+    },
+    finishedBlack: {
+        color: 'black',
+    },
+    continuing: {
+        color: "green",
+    },
+    nextSpeechButton: {
+        color: "#2bc454",
+    }
 })
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
